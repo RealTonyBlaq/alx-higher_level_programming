677ef23650661cb629b44130db3993369ab5c93e
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-09-06 00:04:04 +0100

    identing long print formats in Python

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,10 +6,10 @@ if number < 0:
 else:
     last = number % 10
 if last > 5:
-    print(f"last digit of {number:d} is {last:d} \
-and is greater than 5")
+    print(f"last digit of {number:d} is {last:d}\
+ and is greater than 5")
 elif last < 6 and last != 0:
-    print(f"last digit of {number:d} is {last:d} \
-and is less than 6 and not 0")
+    print(f"last digit of {number:d} is {last:d}\
+ and is less than 6 and not 0")
 else:
     print(f"last digit of {number:d} is {last:d} and is 0")
