94d959197303e6b5e1318e082e86cccb1d93a61b
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-09-08 06:02:59 +0100

    hidden discovery

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     import hidden_4
     lists = dir(hidden_4)
-    for l in lists:
-        if l[:2] != "__":
-            print(l)
+    for c in lists:
+        if c[:2] != "__":
+            print(c)
