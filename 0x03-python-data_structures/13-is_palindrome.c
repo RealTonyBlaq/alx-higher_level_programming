7eae812b84c4d327104ccf91a80a9c7f75689df8
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-09-12 00:14:48 +0100

    function checks if a singly linked list is a palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,49 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - Checks if a linked list
+ * is a palindrome
+ * @head: The head pointer to the list
+ *
+ * Return: 1 if true, else 0
+ */
+
+int is_palindrome(listint_t **head)
+{
+	listint_t *forward = *head, *backward = NULL;
+
+	if (*head == NULL || (*head)->next == NULL)
+		return (0);
+	backward = reverse_list(head);
+	while (forward != NULL && backward != NULL)
+	{
+		if (forward->n != backward->n)
+			return (0);
+		forward = forward->next;
+		backward = backward->next;
+	}
+	return (1);
+}
+
+/**
+ * reverse_list - Function reverses a list
+ * @head: The head pointer to the list
+ *
+ * Return: The reversed list
+ */
+
+listint_t *reverse_list(listint_t **head)
+{
+	listint_t *front = NULL;
+	listint_t *current = *head;
+	listint_t *back = NULL;
+
+	while (current != NULL)
+	{
+		front = current->next;
+		current->next = back;
+		back = current;
+		current = front;
+	}
+	return (back);
+}
