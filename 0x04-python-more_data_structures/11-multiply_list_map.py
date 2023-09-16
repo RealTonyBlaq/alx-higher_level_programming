1474fc543f9d4ed6d682553d44833052f1d60d82
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-09-16 01:30:06 +0100

    multiplies all values in a list by a number without using loops

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,18 +1,3 @@
 #!/usr/bin/python3
-
 def multiply_list_map(my_list=[], number=0):
-
-    """
-    Function:
-    ---------
-    multiply_list_map - Returns a new dictionary with all values multiplied
-                        by number
-
-    Parameters:
-    -----------
-    my_list - The list
-    number - The number
-
-    Return: The updated/new list
-    """
     return (list(map(lambda x: x * number, my_list)))
