c702277c37d77ef7f2cbf2a38ed6660be792f9dd
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-09-17 22:18:24 +0100

    Roman numerals to integer

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+
+    """
+    Function:
+    ---------
+    roman_to_int - Converts roman numerals to integer
+
+    Parameters:
+    -----------
+    roman_string - A string of roman numerals
+
+    Return: The converted number
+    """
