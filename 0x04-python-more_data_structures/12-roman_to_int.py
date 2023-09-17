790163295378c6e3248c78a14bc21b3b4a15affb
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-09-18 00:10:46 +0100

    function converts roman numerals to integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def roman_to_int(roman_string):
 
     """
@@ -13,3 +12,22 @@ def roman_to_int(roman_string):
 
     Return: The converted number
     """
+    if roman_string == "" or roman_string is None:
+        return 0
+    intgr = 0
+    lenn = len(roman_string)
+    dic = dict(I=1, V=5, X=10, L=50, C=100, D=500, M=1000)
+    for i in range(0, lenn):
+        if dic[roman_string[i]]:
+            if i > 0:
+                if (dic[roman_string[i - 1]] < dic[roman_string[i]]):
+                    k = dic[roman_string[i]] - dic[roman_string[i - 1]]
+                    intgr += k - dic[roman_string[i - 1]]
+                else:
+                    intgr += dic[roman_string[i]]
+            else:
+                intgr += dic[roman_string[i]]
+        else:
+            return 0
+
+    return intgr
