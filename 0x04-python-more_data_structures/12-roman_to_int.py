39cf9f7721600dd40dcff2a40d0e752a1b628d0e
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-09-18 01:31:07 +0100

    function converts roman numerals to decimals

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -12,8 +12,6 @@ def roman_to_int(roman_string):
 
     Return: The converted number
     """
-    if roman_string is None:
-        return 0
     intgr = 0
     lenn = len(roman_string)
     dic = dict(I=1, V=5, X=10, L=50, C=100, D=500, M=1000)
