65496e8a63a4f16eac33be50bec42f563e30e9ee
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-09-26 00:02:28 +0100

    div lists and returns a new list

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -24,7 +24,7 @@ def list_division(my_list_1, my_list_2, list_length):
         except ZeroDivisionError:
             print("division by 0")
             new.append(0)
-        except (TypeError, ValueError):
+        except (TypeError):
             print("wrong type")
             new.append(0)
     return new
