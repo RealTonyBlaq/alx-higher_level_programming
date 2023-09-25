ca2c756fbc97fa90bd873950d5c57ea3913ccba7
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-09-25 22:47:06 +0100

    safely prints only integers from a list even though it contains values of type string

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    """
+    safe_print_list_integers - Prints x elements of type (int) from a list
+                            which could contain both integers and strings
+    ------------------------
+    Parameters:
+
+    my_list: The list
+    x: Number of elements to print
+    ------------------------
+
+    Return: Total number of integers printed
+    """
+    count = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except (TypeError, ValueError, IndexError):
+            continue
+    return count
