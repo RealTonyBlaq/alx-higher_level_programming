be26e6024aeb4af8ebd2bbaa54c2dae573eaf57f
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-09-26 00:25:40 +0100

    raise excepttions of TypeError

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def raise_exception():
+    """
+    raise_exception - Function raises a type exception
+    ---------------
+    Parameter:
+
+    Nothing
+    ---------------
+
+    Return: Nothing
+    """
+    raise TypeError
