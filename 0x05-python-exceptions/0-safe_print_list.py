330d0566fa3c250ad96f8ea82819157c04d8a731
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-09-25 22:35:04 +0100

    safely print x elements from a list even if x > len(my_list)

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -12,8 +12,6 @@ def safe_print_list(my_list=[], x=0):
     -----------
     Return: Count of printed elements
     """
-    if my_list == []:
-        return 0
     count = 0
     for i in range(x):
         try:
