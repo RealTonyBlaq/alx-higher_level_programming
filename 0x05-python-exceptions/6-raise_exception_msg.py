ffc7697c40dca72878b8d3fb6c6c4f7af14a96cf
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-09-26 00:33:22 +0100

    raise me a nameError exception

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def raise_exception_msg(message=""):
+    """
+    raise_exception_msg - Raises a name exception with a message
+    --------------------
+    Parameter:
+
+    message: The message
+    --------------------
+
+    Return: Nothing
+    """
+    raise NameError(message)
