38cd2a909664499c67d9f22d48875fa23e35e3b7
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-09-25 23:05:49 +0100

    safe print divs

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    """
+    safe_print_division - Safely prints the result of a division
+    --------------------
+    Parameters:
+
+    a: The numerator
+    b: The denominator
+    --------------------
+
+    Return: The result of the division
+    """
+    try:
+        result = a / b
+    except (ValueError, ZeroDivisionError):
+        return None
+    finally:
+        print("Inside result: {}".format(result))
+        return result
