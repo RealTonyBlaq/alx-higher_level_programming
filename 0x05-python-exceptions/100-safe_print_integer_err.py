a142a74df0b7a00c8435b7673b9e6b6b492274f9
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-09-26 00:52:29 +0100

    safe print integer

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+def safe_print_integer_err(value):
+    """
+    safe_print_integer_err - Prints an integer
+    ----------------------
+    Parameter:
+
+    value: The integer
+    ----------------------
+
+    Return: True, if value was correctly printed
+            else;
+            False, and prints in stderr the error
+    """
+    import sys
+    try:
+        print("{:d}".format(value))
+        return True
+    except TypeError as err:
+        sys.stderr.write("Exception: ")
+        sys.stderr.write(err)
+        sys.stderr.write("\n")
+        return False
