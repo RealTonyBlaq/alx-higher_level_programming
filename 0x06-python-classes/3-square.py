1c0933ece755429ce1365856437136bf2e100249
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-09-27 20:46:57 +0100

    define a class Square, an private instance attribute size, and a public method area

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -15,7 +15,6 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-    @property
     def area(self):
         """ Returns the area of the square (size^2) """
         return self.__size ** 2
