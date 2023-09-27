a6e9ccfe00a9f809fd464c7670d6c3ba17548442
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-09-27 19:02:13 +0100

    class square and init size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -4,6 +4,7 @@ class Square:
     """
     Defines a square class
     with an __init__ method
+
     """
     def __init__(self, size):
         """ Initializes a private attribute size of class Square """
