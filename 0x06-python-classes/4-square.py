5add6e76bbf708c4851f82857427a9cd5a722131
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-09-27 21:12:26 +0100

    defines a class Square, a private instance attr size, a public method area, a setter size

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" Defines a class Square """
+
+
+class Square:
+    """
+    Defines a square class
+    with an __init__ method
+
+    """
+    def __init__(self, size=0):
+        """ Initializes a private attribute (size) of class Square """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """ Returns the area of the square (size^2) """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """ Gets the value size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ Sets self.__size to equal size """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
