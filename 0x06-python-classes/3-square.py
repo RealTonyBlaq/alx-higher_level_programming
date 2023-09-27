cb8be11080717f27297c02aa105aceab09814a88
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-09-27 20:41:36 +0100

    defines a method area that returns size ** 2

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Defines a class Square """
+
+
+class Square:
+    """
+    Defines a square class
+    with an __init__ method
+
+    """
+    def __init__(self, size=0):
+        """ Initializes a private attribute (size) of class Square """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    @property
+    def area(self):
+        """ Returns the area of the square (size^2) """
+        return self.__size ** 2
