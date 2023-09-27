c53d41d7fc00e3ffaea8bffcad68d55c77e3e974
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-09-27 19:15:46 +0100

    square class, with a private instance attribute

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -6,6 +6,10 @@ class Square:
     with an __init__ method
 
     """
-    def __init__(self, size):
+    def __init__(self, size=0):
         """ Initializes a private attribute size of class Square """
+        if not isinstance(size, int):
+            raise TypeError("Size must be an integer")
+        elif size < 0:
+            raise ValueError("Size must be >= 0")
         self.__size = size
