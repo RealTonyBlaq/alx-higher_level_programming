6408ee0b11604d2de43499134f841b45e528d5bd
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-10-10 16:54:59 +0100

    prints a sorted list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""  Child class (Mylist) inherits from List """
+
+
+class MyList(list):
+    """ Define a subclass Mylist """
+
+    def print_sorted(self):
+        """ prints a sorted list """
+        print(sorted(list))
