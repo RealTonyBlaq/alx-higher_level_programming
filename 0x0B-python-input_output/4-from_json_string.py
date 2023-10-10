d6942ea26d424b29d2a36e4951857ddc86d617c9
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-10-10 22:02:23 +0100

    from json obj to python object

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+""" Module for JSON representation """
+
+
+def from_json_string(my_str):
+    """
+    to_json_string - Returns an object represented by a JSON string
+    --------------
+
+    Parameter:
+
+    my_obj: The object
+    --------------
+
+    Return: Python object
+    """
+    import json as j
+
+    return j.load(my_str)
