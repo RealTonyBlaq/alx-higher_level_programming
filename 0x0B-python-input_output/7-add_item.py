26f5dcbe6d5bc16ff11a016960cf50240dcbcfe8
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-10-11 21:40:51 +0100

    load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,13 +7,11 @@ loader = __import__('6-load_from_json_file').load_from_json_file
 saver = __import__('5-save_to_json_file').save_to_json_file
 
 filename = "add_item.json"
-av.pop(0)
+my_list = []
 try:
     my_list = loader(filename)
-    if my_list is None:
-        saver(av, filename)
-    else:
-        my_list.extend(av)
-        saver(my_list, filename)
+    for i in range(1, len(av)):
+        my_list.append(av[i])
+    saver(my_list, filename)
 except FileNotFoundError:
-    saver(av, filename)
+    saver(my_list, filename)
