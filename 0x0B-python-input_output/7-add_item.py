b730f41ef453c1eb0f931a602b86e86c37d15022
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-10-12 08:40:26 +0100

    fix

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ Module for concatenating arguments to a list """
+
 from sys import argv
 loader = __import__('6-load_from_json_file').load_from_json_file
 saver = __import__('5-save_to_json_file').save_to_json_file
