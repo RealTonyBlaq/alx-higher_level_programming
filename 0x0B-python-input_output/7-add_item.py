6663e85136ae907232e1b09be84c8d73cbfede6f
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-10-12 08:09:04 +0100

    load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
 """ Module for concatenating arguments to a list """
-
 from sys import argv
+loader = __import__('6-load_from_json_file').load_from_json_file
+saver = __import__('5-save_to_json_file').save_to_json_file
 
 
-if __name__ == "__main__":
-    loader = __import__('6-load_from_json_file').load_from_json_file
-    saver = __import__('5-save_to_json_file').save_to_json_file
-
-    filename = "add_item.json"
-    try:
-        my_list = loader(filename)
-    except FileNotFoundError:
-        my_list = []
-    my_list.extend(argv[1:])
-    saver(my_list, filename)
+filename = "add_item.json"
+try:
+    my_list = loader(filename)
+except FileNotFoundError:
+    my_list = []
+my_list.extend(argv[1:])
+saver(my_list, filename)
