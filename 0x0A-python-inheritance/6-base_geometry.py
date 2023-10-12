7efd81c78a423afd8ae78355a8cd42a7d0be069a
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-10-12 05:38:17 +0100

    def area

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+""" Module contains an empty class """
+
+
+class BaseGeometry:
+    """ Define an empty class """
+
+    def area(self):
+        """ Public instance method """
+        raise Exception("area() is not implemented")
