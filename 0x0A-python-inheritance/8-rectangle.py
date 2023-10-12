0b1137f4e5fceabd03122d174e749201c717808d
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-10-12 06:14:13 +0100

    intgr vaildator

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -7,19 +7,7 @@ from the BaseGeometry class
 """
 
 
-class BaseGeometry:
-    """ Define a class """
-
-    def area(self):
-        """ Public instance method """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """ Method validates value """
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0". format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
@@ -27,6 +15,8 @@ class Rectangle(BaseGeometry):
 
     def __init__(self, width, height):
         """ Initializes the private attributes """
+        self.integer_validator("width", width)
         self.__width = width
+        self.integer_validator("height", height)
         self.__height = height
         super().__init__()
