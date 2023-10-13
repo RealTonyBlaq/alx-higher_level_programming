1ba1945bebdecfdf3abceda57238b8d4533f2b92
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-10-13 15:44:03 +0100

    class with inverted operators

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Module for class MyInt that inherits from 'int' """
+
+
+class MyInt(int):
+    """ Define the rebel class MyInt """
+
+    def __eq__(self, other):
+        """
+        Returns (inverted):
+        --------
+        True: if self != other
+        False: if self == other
+        """
+        return not super().__eq__(other)
+
+    def __ne__(self, other):
+        """
+        Returns (inverted):
+        --------
+        True: if self == other
+        False: if self != other
+        """
+        return not super().__ne__(other)
