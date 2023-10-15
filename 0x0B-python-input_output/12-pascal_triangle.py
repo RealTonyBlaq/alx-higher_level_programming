33435928e5235fdfaa8f4d2421e574df8febde4b
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-10-15 01:09:42 +0100

    pascal triangle

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -15,14 +15,12 @@ def pascal_triangle(n):
     Return: A list of lists (Matrix)
     """
     pasc = []
-    if n <= 0:
-        return pasc
-    for i in range(n):
-        pasc.append([1])
-        if i > 1:
-            for k in range(len(pasc[i - 1])):
-                if k > 0:
+    if n > 0:
+        for i in range(n):
+            pasc.append([1])
+            if i > 1:
+                for k in range(1, len(pasc[i - 1])):
                     pasc[i].append(pasc[i - 1][k - 1] + pasc[i - 1][k])
-        if i != 0:
-            pasc[i].append(1)
+            if i > 0:
+                pasc[i].append(1)
     return pasc
