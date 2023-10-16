3f05b703d5bab88251ef124ba64435e80f4ea34a
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-10-16 16:50:26 +0100

    square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,7 +9,7 @@ class Square(Rectangle):
 
     def __init__(self, size, x=0, y=0, id=None):
         """
-        Initializes the attributes
+        Initializes the attributes based on Rectangle
 
         Parameters:
         -----------
@@ -25,3 +25,18 @@ class Square(Rectangle):
         """ Returns the string representation of Square """
         return "[Square] ({}) {}/{} - {}"\
             .format(self.id, self.x, self.y, self.width)
+    
+    @property
+    def size(self):
+        """ Returns the value for size """
+        return self.width
+    
+    @size.setter
+    def size(self, value):
+        """ Sets size to value """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.width = value
+        self.height = value
