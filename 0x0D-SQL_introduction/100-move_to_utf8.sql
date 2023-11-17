b42fdae9ad624c67337963cbf5d174cedaf4ce8c
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-11-17 01:45:33 +0100

    change database, table, field _name_ to utf-8

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,11 @@
+-- Script moves/converts database, columns, fields to 'utf-8'
+USE hbtn_0c_0
+
+-- convert the database to utf-8
+ALTER DATABASE CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Convert tables to utf-8
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Convert fields in tables to utf-8
+ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
