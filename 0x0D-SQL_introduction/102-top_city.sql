199bd11ff0114b47585bb24412a55b80dee61443
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-11-17 20:22:39 +0100

    t0p 3

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,7 +1,7 @@
 -- script lists the top 3 cities ordered by temperature
 SELECT city, AVG(value) AS avg_temp
 FROM temperatures
-GROUP BY city
 WHERE month BETWEEN 7 AND 8
+GROUP BY city
 ORDER BY avg_temp DESC
 LIMIT 3;
