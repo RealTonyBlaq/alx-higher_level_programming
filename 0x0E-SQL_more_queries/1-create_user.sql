bcbc57062a2e93ea88b791215df72844bc8f4c4c
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-11-18 21:43:43 +0100

    fix

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,4 @@
 -- Script creates a user with a password and grants all privileges
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
 IDENTIFIED WITH mysql_native_password BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
