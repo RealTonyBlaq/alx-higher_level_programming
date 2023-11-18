2b88e4daebc588eb87226016182eae0e102017c8
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-11-18 23:47:10 +0100

    join & join

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,9 @@
+-- script lists all the cities in California contained in tables
+SELECT *
+FROM cities
+WHERE state_id = (
+    SELECT id
+    FROM states
+    WHERE name = "California"
+)
+ORDER BY cities.id;
