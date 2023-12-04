3db319ec3335d98768235a4a15c6ed9e45410a9a
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-12-04 16:56:07 +0100

    FIX

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,13 +1,12 @@
 -- Script lists all genres not linked to the show "Dexter"
 SELECT tv_genres.name
 FROM tv_genres
-INNER JOIN (
-	SELECT tv_show_genres.genre_id
+RIGHT JOIN (
+	SELECT tv_show_genres.genre_id AS id
 	FROM tv_show_genres
 	INNER JOIN tv_shows
 	ON tv_shows.id = tv_show_genres.show_id
 	WHERE tv_shows.title = 'Dexter'
 ) AS new_table
-ON tv_genres.genre_id = new_table
+ON tv_genres.id = new_table.id
 ORDER BY tv_genres.name ASC;
-
