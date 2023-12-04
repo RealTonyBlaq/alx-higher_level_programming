ef5a2f81fa209f0924bc3f520d491cd97b214639
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-12-04 16:46:40 +0100

    fixing

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,5 +1,13 @@
 -- Script lists all genres not linked to the show "Dexter"
 SELECT tv_genres.name
 FROM tv_genres
-NATURAL JOIN tv_show_genres
-ON tv_show_genres.genre_id = tv_genres.id
+INNER JOIN (
+	SELECT tv_show_genres.genre_id
+	FROM tv_show_genres
+	INNER JOIN tv_shows
+	ON tv_shows.id = tv_show_genres.show_id
+	WHERE tv_shows.title = 'Dexter'
+)
+ON tv_genres.genre_id = genre_id
+ORDER BY tv_genres.name ASC;
+
