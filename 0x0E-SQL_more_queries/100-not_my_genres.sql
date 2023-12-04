332e2711494a72303534e031eb2006df22c8c765
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-12-04 16:24:50 +0100

    fix

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -3,7 +3,7 @@ SELECT tv_genres.name
 FROM tv_genres
 INNER JOIN tv_show_genres
 ON tv_show_genres.genre_id = tv_genres.id
-WHERE tv_show_genres.id = (
+WHERE tv_show_genres.genre_id = (
 	SELECT id
 	FROM tv_shows
 	WHERE title = 'Dexter'
