090734e33e3d20019dde56b3191d8440794a3c91
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-12-04 21:38:51 +0100

    non-comedy

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -2,5 +2,11 @@
 SELECT tv_shows.title
 FROM tv_shows
 LEFT JOIN (
-	SELECT 
-)
+	SELECT tv_show_genres.show_id AS id
+	FROM tv_show_genres
+	INNER JOIN tv_genres
+	ON tv_genres.id = tv_show_genres.genre_id
+	WHERE tv_genres.name != 'Comedy'
+) AS new_table
+ON tv_shows.id = new_table.id
+ORDER BY tv_shows.title ASC;
