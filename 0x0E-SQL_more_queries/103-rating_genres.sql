0d718413b7af701bbbaf7373d386f8ab9b6d69e8
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-12-04 22:23:51 +0100

    show genres by rating

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,4 +1,4 @@
--- Script lists all genres in the database by their rating
+-- Script lists all genres in the database "hbtn_0d_tvshows_rate "by their rating
 SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating
 FROM tv_genres
 INNER JOIN tv_show_genres
