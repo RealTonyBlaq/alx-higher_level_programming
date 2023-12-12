cebac2f08b231267ddb0bc0564681bd3d261c3da
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-12-12 14:06:12 -0800

    multi print

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 /* Script prints mulitple strings */
-console.log('C is fun\nPython is cool\nJavascript is amazing');
+console.log('C is fun\nPython is cool\nJavaScript is amazing');
