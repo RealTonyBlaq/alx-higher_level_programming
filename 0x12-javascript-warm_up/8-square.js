e30a15b52742e44b7652b10dbd2c791c3ea28100
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-12-14 00:43:25 +0100

    fix

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -6,9 +6,9 @@ const size = parseInt(process.argv[2]);
 if (!isNaN(size)) {
   let i = 0;
   while (i < size) {
-        console.log('X'.repeat(size));
+    console.log('X'.repeat(size));
     i++;
   }
 } else {
-    console.log('Missing size');
+  console.log('Missing size');
 }
