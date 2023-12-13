02d45a258e8a2734f914e71cafc294611feb95e0
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-12-13 23:57:28 +0100

    to integer and beyond

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
 /* Script converts args to integers */
-if (type(Process.argv[2]) === Number) {
-  console.log('My number: ', process.argv[2]);
+const arg = parseInt(process.argv[2], 10);
+
+if (!isNaN(arg)) {
+  console.log('My number: ' + arg);
 } else {
   console.log('Not a number');
 }
