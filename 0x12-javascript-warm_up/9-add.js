64f0c3f703dd08cb478bedcbf6dd1f497a95f17c
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-12-14 00:58:41 +0100

    add me up args

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -8,8 +8,4 @@ function add (a, b) {
 const firstArg = parseInt(process.argv[2]);
 const secondArg = parseInt(process.argv[3]);
 
-if (isNaN(firstArg) || isNaN(secondArg)) {
-  console.log('Nan');
-} else {
-  console.log(add(firstArg, secondArg));
-}
+console.log(add(firstArg, secondArg));
