661352d52386539c8a86fcfadffce1da270c7b13
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-12-17 22:32:19 +0100

    finds the second largest argument passed

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 /* Script finds the maximum number in the list of arguments */
 
-let arg_list = [];
+let argList = [];
 if (!isNaN(process.argv[2])) {
   while (process.argv[i]) {
-    arg_list.push(process.argv[i]);
+    argList.push(process.argv[i]);
     i++;
   }
-  let maxNumber = Math.max(...arg_list);
-  let args = arg_list.filter(arg_list => arg_list !== maxNumber);
+  let maxNumber = Math.max(...argList);
+  let args = argList.filter(argList => argList !== maxNumber);
   console.log(Math.max(args));
 } else {
     console.log(0);
