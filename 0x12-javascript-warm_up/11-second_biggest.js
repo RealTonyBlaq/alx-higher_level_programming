54cd7497928deaeb4551f5a49c2fb4b0d62b412c
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-12-17 22:35:07 +0100

    fix camelCase issues

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,14 +1,15 @@
 #!/usr/bin/node
 /* Script finds the maximum number in the list of arguments */
 
-let argList = [];
+const argList = [];
 if (!isNaN(process.argv[2])) {
+  let i = 2;
   while (process.argv[i]) {
     argList.push(process.argv[i]);
     i++;
   }
-  let maxNumber = Math.max(...argList);
-  let args = argList.filter(argList => argList !== maxNumber);
+  const maxNumber = Math.max(...argList);
+  const args = argList.filter(argList => argList !== maxNumber);
   console.log(Math.max(args));
 } else {
     console.log(0);
