f40ba4dd34292d50be79269376f619b9460b3463
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-12-17 22:06:34 +0100

    buidl

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/* Script finds the maximum number in the list of arguments */
+
+let arg_list = [];
+if (!isNaN(process.argv[2])) {
+  for (let i = 2; i >= 2; i++) {
+    arg_list.push(process.argv);
+  }
+} else {
+    
+}
