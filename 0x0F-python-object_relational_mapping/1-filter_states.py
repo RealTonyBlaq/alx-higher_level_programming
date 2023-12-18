0b9011ad594bed76e0bbb226b06cedd20e5fafe7
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-12-18 23:06:39 +0100

    forced case sensitivity

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     cur = database.cursor()
     cur.execute("SELECT *\
                 FROM states\
-                WHERE name LIKE 'N%'\
+                WHERE name LIKE BINARY 'N%'\
                 ORDER BY states.id ASC;"
                 )
     data = cur.fetchall()
