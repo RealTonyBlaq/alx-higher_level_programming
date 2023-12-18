51ab9df70de4f18c040288eb0ae8a67eb2f0da9b
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-12-18 23:29:11 +0100

    buidl

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,4 +8,5 @@ if __name__ == "__main__":
     database = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
                                passwd=argv[1], db=argv[3])
     cur = database.cursor()
-    
+    cur.execute('SELECT *\
+                FROM states')
