5500954d435fda755837c4e0169e5fb22b598c90
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-12-18 23:27:06 +0100

    buidl

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Module """
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    database = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
+                               passwd=argv[1], )
