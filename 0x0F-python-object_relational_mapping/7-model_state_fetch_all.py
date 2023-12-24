4797f13d059bb466b936c6f262e677b9c8183510
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-12-24 22:01:22 +0100

    buidl

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -8,4 +8,4 @@ if __name__ == "__main__":
     engine = create_engine("mysql://{}:{}@localhost/{}"
                            .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
     conn = engine.connect()
-    
+    statement = select([State])
