b1064ebab7b152e3b243bde15e011e13abfb7302
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-12-24 21:58:46 +0100

    buidl

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 from model_state import Base, State
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, MetaData
 from sys import argv
 
 if __name__ == "__main__":
