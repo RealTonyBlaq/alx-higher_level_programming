11ecac31d4e03f7f95b85cc99a27b2d284ed6405
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-12-24 19:11:32 +0100

    buidl

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,6 +2,8 @@
 
 from sqlalchemy import Table, String, MetaData, Integer
 from sqlalchemy.ext.declarative import declarative_base
-from 
+from sqlalchemy import create_engine
+
+engine = create_engine()
 
 
