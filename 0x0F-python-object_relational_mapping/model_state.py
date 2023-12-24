9d3b36295bef1e451b0a1ff3d0e685d94962b641
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-12-24 19:12:49 +0100

    buidl

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -4,6 +4,6 @@ from sqlalchemy import Table, String, MetaData, Integer
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 
-engine = create_engine()
+engine = create_engine('mysql://root:root@localhost/')
 
 
