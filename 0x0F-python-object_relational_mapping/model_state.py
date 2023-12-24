b7405c59c56c85064e0a0aaaef3569933d332c41
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-12-24 20:15:41 +0100

    buidl

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -7,6 +7,8 @@ from sqlalchemy import create_engine
 
 engine = create_engine('mysql://root:root@localhost/hbtn_0e_6_usa', pool_pre_ping=True)
 Base = declarative_base()
+meta = MetaData()
 
 class States(Base):
-    """ Defining a class States which is a Table"""
+    """ Defining a class States which inherits from Base """
+    id = Table("id", meta, )
