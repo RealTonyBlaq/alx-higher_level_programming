679e435b54f168b06c0765dbcf5fad5e8acf692d
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-12-24 22:19:21 +0100

    buidl

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -2,13 +2,14 @@
 """ Script queries a database """
 
 from model_state import Base, State
-from sqlalchemy import create_engine, MetaData, select
+from sqlalchemy import create_engine, select
 from sys import argv
 
 if __name__ == "__main__":
     engine = create_engine("mysql://{}:{}@localhost/{}"
                            .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
     conn = engine.connect()
+    Base.metadata.create_all(engine)
     statement = select(State.__tablename__)
     result = conn.execute(statement)
     for row in result:
