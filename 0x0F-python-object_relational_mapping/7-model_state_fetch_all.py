ef31ff8d302f3a50d9650bc6b337c68912ef973a
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-12-24 22:00:04 +0100

    buidl

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
 from model_state import Base, State
-from sqlalchemy import create_engine, MetaData
+from sqlalchemy import create_engine, MetaData, select
 from sys import argv
 
 if __name__ == "__main__":
     engine = create_engine("mysql://{}:{}@localhost/{}"
                            .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    conn = engine.connect()
     
