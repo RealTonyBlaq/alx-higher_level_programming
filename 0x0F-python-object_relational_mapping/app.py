421064abf023fbfd8e8c03d1bdea5da68c647520
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-12-24 18:36:45 +0100

    buidl

--- 0x0F-python-object_relational_mapping/app.py
+++ 0x0F-python-object_relational_mapping/app.py
@@ -5,13 +5,13 @@ from sqlalchemy import MetaData, Table, Column, String, Integer
 
 engine = create_engine("mysql://root:root@localhost/hbtn_0d_tvshows")
 
-metadata = MetaData('engine')
+metadata = MetaData(bind=engine)
 
 table = Table("new", metadata,
               Column('id', Integer, primary_key=True),
               Column('name', String(50)),
               Column('email', String(70)))
 metadata.create_all()
-result = metadata.tables()
+result = metadata.tables().keys()
 for row in result:
     print(f'my table: {row}')
