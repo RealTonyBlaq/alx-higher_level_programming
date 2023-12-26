e0c6c2af5d63e7d4868ec393c91cad93bcdb8864
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-12-26 13:24:34 +0100

    building a sql query using python

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -4,11 +4,11 @@
 
 from model_city import Base, City
 from model_state import Base, State
-from sqlalchemy import create_engine, select, join
+from sqlalchemy import create_engine, select
 from sys import argv
 
 if __name__ == "__main__":
     engine = create_engine('mysql://{}:{}@localhost:3306/{}'.
                         format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
     with engine.connect() as connection:
-        statement = select(State.name, City.id, City.name).j
+        statement = select(State.name, City.id, City.name).join()
