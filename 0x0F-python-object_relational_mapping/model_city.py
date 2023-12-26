0286583c09defad8674a358b681c177de2352eed
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-12-26 12:40:08 +0100

    buidl

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -10,4 +10,5 @@ class City(Base):
     """ Defining the class City """
     __tablename__ = 'cities'
     id = Column('id', Integer, nullable=False, primary_key=True, unique=True)
-    name = Column('name', String(128), nullable=)
+    name = Column('name', String(128), nullable=False)
+    state_id = Column('state_id', Integer, )
