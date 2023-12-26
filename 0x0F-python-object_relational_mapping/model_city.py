e16e46609550384884b7f3fe80cc882c2744a77d
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-12-26 12:38:51 +0100

    buidl

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -9,5 +9,5 @@ from sqlalchemy import Table, Column, String, Integer
 class City(Base):
     """ Defining the class City """
     __tablename__ = 'cities'
-    id = Column('id', Integer, nullable=False, primary_key=True)
-    
+    id = Column('id', Integer, nullable=False, primary_key=True, unique=True)
+    name = Column('name', String(128), nullable=)
