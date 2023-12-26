47e0df968b8fdf254423b9775e44400f448bdf72
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-12-26 13:41:01 +0100

    fixed pycodestyle

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
-""" Module """
+"""
+Script queries two tables 'cities' and 'states'
+on states.id == cities.state_id and prints the result
+in a pretty format
+"""
 
 from model_city import Base, City
 from model_state import Base, State
@@ -8,10 +12,12 @@ from sqlalchemy import create_engine, select
 from sys import argv
 
 if __name__ == "__main__":
-    engine = create_engine('mysql://{}:{}@localhost:3306/{}'.
-                        format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql://{}:{}@localhost:3306/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
     with engine.connect() as connection:
-        statement = select(State.name, City.id, City.name).join(City, State.id == City.state_id)
+        statement = select(State.name, City.id,
+                           City.name).join(City, State.id == City.state_id)
         result = connection.execute(statement)
         for row in result:
             print("{}: ({}) {}".format(row[0], row[1], row[2]))
