82dbfb900e177a471e3ecdfdcb5b9a7b3ed854a2
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-12-26 13:18:02 +0100

    building a sql query using python

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -8,4 +8,5 @@ from sys import argv
 
 engine = create_engine('mysql://{}:{}@localhost:3306/{}'.
                        format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
-conn = engine.connect()
+with engine.connect() as connection:
+    
