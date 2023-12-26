393f766a299c132209ec662ed01f223dfa6c56c0
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-12-26 12:54:31 +0100

    buidl

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -17,9 +17,11 @@ class City(Base):
     id (int): An auto-generated column with unique values, a primary key
             and non-null
     name (str): A string column of max 128 characters and cannot be null
-    state_id (int): A column that cannot be null and is a foreign key to states.id
+    state_id (int): A column that cannot be null and is a foreign key
+            to states.id
     """
     __tablename__ = 'cities'
     id = Column('id', Integer, nullable=False, primary_key=True, unique=True)
     name = Column('name', String(128), nullable=False)
-    state_id = Column('state_id', Integer, ForeignKey('states.id'), nullable=False)
+    state_id = Column('state_id', Integer, ForeignKey('states.id'),
+                      nullable=False)
