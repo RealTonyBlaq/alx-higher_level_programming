930da23d3202612ab64738e8c1d1e3806dd9eb7c
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-12-26 10:05:22 +0100

    buidl

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -7,20 +7,11 @@ If successful, the id is printed, otherwise 'Not found'
 """
 
 from model_state import Base, State
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine, MetaData
 from sys import argv
 
 if __name__ == "__main__":
     engine = create_engine("mysql://{}:{}@localhost:3306/{}"
                            .format(argv[1], argv[2], argv[3]),
                            pool_pre_ping=True)
-    Session = sessionmaker(engine)
-    session = Session()
-
-    result = session.query(State).filter(State.name == '{}'
-                                         .format(argv[4])).first()
-    try:
-        print(result.id)
-    except AttributeError:
-        print("Not found")
+    
