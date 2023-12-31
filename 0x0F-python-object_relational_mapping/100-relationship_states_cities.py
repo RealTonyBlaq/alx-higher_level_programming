97e59818de1e76eacbe7193c88f259a594b6c358
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-12-31 18:50:25 +0100

    buidl

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -7,4 +7,6 @@ from relationship_city import Base, City
 from relationship_state import Base, State
 from sys import argv
 
-engine = create_engine("mysql://{}:{}")
+engine = create_engine("mysql://{}:{}@localhost:3306/{}"
+                       .format(argv[1], argv[2], argv[3]),
+                       pool_pre_ping=)
