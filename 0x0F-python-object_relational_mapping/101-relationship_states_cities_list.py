58061ff2b88deeb78890a06ebfb6ad8daf3d782e
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-06 18:36:53 +0100

    fix my code

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -17,6 +17,8 @@ if __name__ == "__main__":
                            .format(argv[1], argv[2], argv[3]),
                            pool_pre_ping=True)
 
+    Base.metadata.create_all(engine)
+
     Session = sessionmaker(bind=engine)
     session = Session()
 
