28c5100ee1af361d65083a2be082f6279c3d2ffb
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-06 15:52:06 +0100

    fixed code to run when not imported

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
-""" Script creates a new state with a new city which have a relationship """
+"""
+Script creates a new state with a new city which have a relationship
+
+This script will not be executed when imported
+"""
 
 from sqlalchemy import create_engine
 from relationship_city import City
@@ -8,18 +12,19 @@ from relationship_state import Base, State
 from sys import argv
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine("mysql://{}:{}@localhost:3306/{}"
-                       .format(argv[1], argv[2], argv[3]),
-                       pool_pre_ping=True)
+if __name__ == "__main__":
+    engine = create_engine("mysql://{}:{}@localhost:3306/{}"
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
 
-Session = sessionmaker(engine)
-session = Session()
-Base.metadata.create_all(engine)
+    Session = sessionmaker(engine)
+    session = Session()
+    Base.metadata.create_all(engine)
 
-new_state = State(name='California')
-new_city = City(name='San Francisco')
-new_state.cities.append(new_city)
+    new_state = State(name='California')
+    new_city = City(name='San Francisco')
+    new_state.cities.append(new_city)
 
-session.add(new_state)
-session.commit()
-session.close()
+    session.add(new_state)
+    session.commit()
+    session.close()
