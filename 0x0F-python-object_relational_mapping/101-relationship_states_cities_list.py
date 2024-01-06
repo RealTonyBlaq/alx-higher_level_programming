8f4133bd610a89b58ebf5354dab8ff05ae8b6127
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-06 16:29:44 +0100

    buidl

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -20,4 +20,5 @@ if __name__ == "__main__":
     with engine.connect() as connection:
         statement = select(State.id, State.name)
         statement2 = select(City.id, City.name).join(State, State.id == City.state_id)
-        
+        state_resultconnection.execute(statement)
+
