fd9d582b851370baaebfa136d9e823de0df26073
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-06 17:44:00 +0100

    fixing code to use just one query

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -28,5 +28,6 @@ if __name__ == "__main__":
                 print("{}: {}".format(row[0], row[1]))
                 print("\t{}: {}".format(row[2], row[3]))
                 copy = row[0]
-            if 
+                flag = 1
+            else
 
