f087240bf64bf0968b06d413a83256ca914629bd
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-06 18:31:59 +0100

    fixed pycodestyle

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -16,12 +16,12 @@ if __name__ == "__main__":
     engine = create_engine("mysql://{}:{}@localhost:3306/{}"
                            .format(argv[1], argv[2], argv[3]),
                            pool_pre_ping=True)
-    Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    result = session.query(State).outerjoin(City).order_by(State.id, City.id).all()
+    result = session.query(State).outerjoin(City)\
+        .order_by(State.id, City.id).all()
 
     for state in result:
         print("{}: {}".format(state.id, state.name))
