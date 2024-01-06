c10900a818e1b26cb59eca811f192c05c7798e16
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-06 18:35:20 +0100

    code fix

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -11,6 +11,7 @@ from relationship_city import City
 from relationship_state import Base, State
 from sys import argv
 
+
 if __name__ == "__main__":
     engine = create_engine("mysql://{}:{}@localhost:3306/{}"
                            .format(argv[1], argv[2], argv[3]),
