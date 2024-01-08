86910b959c6d658a21b8b33ea47fb0e46002c5e8
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-08 22:28:51 +0100

    loopie

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+/* Script prints strings using a loop */
+
+const myArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let i = 0; i < myArray.length; i++) {
+  console.log(myArray[i]);
+}
