3397f52bb62e471a8fcc44a33ba30e0c9d4ca471
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-08 22:45:09 +0100

    fix string

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,7 +2,7 @@
 
 /* Script prints strings using a loop */
 
-const myArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < myArray.length; i++) {
   console.log(myArray[i]);
 }
