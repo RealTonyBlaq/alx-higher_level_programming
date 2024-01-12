fa4e13ad4e5fecf564450a3a20d5b0cfe3ad258b
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-12 04:07:48 +0100

    fix semistandard

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -3,14 +3,14 @@
 /* Script counts the number of occurrences of an element in a list */
 
 exports.nbOccurences = function (list, searchElement) {
-  if (! list || list.length === 0 || ! searchElement) {
+  if (!list || list.length === 0 || !searchElement) {
     return 0;
   }
   let count = 0;
   for (let i = 0; i < list.length; i++) {
     if (list[i] === searchElement) {
-        count++;
+      count++;
     }
   }
   return count;
-}
+};
