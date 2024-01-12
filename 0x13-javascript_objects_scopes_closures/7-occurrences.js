951ae92ffde9bddb687dec75d6f6d61116c579c1
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-12 04:03:05 +0100

    buidl

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -7,4 +7,9 @@ exports.nbOccurences = function (list, searchElement) {
     return 0;
   }
   let count = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) {
+        count++;
+    }
+  }
 }
