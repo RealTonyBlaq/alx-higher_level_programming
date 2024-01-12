a01bfdea487a29f6be67836999590bfb25a314cd
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-12 04:01:49 +0100

    buidl

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -3,7 +3,8 @@
 /* Script counts the number of occurrences of an element in a list */
 
 exports.nbOccurences = function (list, searchElement) {
-  if (!list || list.length === 0 || ! searchElement) {
-    
+  if (! list || list.length === 0 || ! searchElement) {
+    return 0;
   }
+  let count = 0;
 }
