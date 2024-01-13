fdaa1d6106d0f1602180539eedc402748f417eae
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-13 21:43:19 +0100

    buidl

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 
-""" Script lists all City objects from the database using"""
+"""
+Script lists all City objects from the database using
+the relationship between City and State
+"""
+
+from relationship_city import City
+from relationship_state import Base, State
+from sqlalchemy
