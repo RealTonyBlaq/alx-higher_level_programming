5b1a3f290fb1f412b91cdf94499a451770ce03ba
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-13 21:58:56 +0100

    buidl

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -13,7 +13,7 @@ from sys import argv
 
 if __name__ == "__main__":
     engine = create_engine("mysql://{}:{}@localhost:3306/{}"
-                           .format(argv[0], argv[1], argv[2]),
+                           .format(argv[1], argv[2], argv[3]),
                            pool_pre_ping=True)
     Session = sessionmaker(engine)
     session = Session()
