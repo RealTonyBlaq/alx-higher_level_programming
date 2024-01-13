9a66d95542fe9bae84c0196f3aded56ccca5970d
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-13 21:47:34 +0100

    buidl

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -8,7 +8,11 @@ the relationship between City and State
 from relationship_city import City
 from relationship_state import Base, State
 from sqlalchemy import create_engine
+
 from sys import argv
 
 if __name__ == "__main__":
-    engine = create_engine("mysql://{}:{}@localhost:3306/{}".format(argv[0], argv[1], argv))
+    engine = create_engine("mysql://{}:{}@localhost:3306/{}"
+                           .format(argv[0], argv[1], argv[2]),
+                           pool_pre_ping=True)
+    
