e27f657a684f3029eb7fb339306f1d06b2a79cc4
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-28 17:22:44 +0100

    buidl

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -11,8 +11,4 @@ url="$1"
 # Send a request using curl and display the size of the response body
 body_size=$(curl -sI "$url" | grep -i '^Content-Length:' | awk '{print $2}' | tr -d '\r')
 
-if [ -z "$body_size" ]; then
-    echo "Unable to determine the size of the response body."
-else
-    echo "Size of the response body: ${body_size} bytes"
-fi
+
