2d0aecbc5b2028333dfe57df2bca02acd82b33c2
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-28 18:16:09 +0100

    buidl

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script takes in a URL, sends a GET request to the URL, and displays the body of the response
-if [ "$(curl -sI "$1" | head -n 1 | cut -d ' ' -f2)" -eq 200 ]; then curl -sX GET "$1"; fi
+if [ "$(curl -sI "$1" | head -n 1 | cut -d ' ' -f2)" -eq 200 ]; then curl -s -L "$1"; fi
