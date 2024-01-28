5ebe98ca869fb5ea110bee0a2909dae47ef8836d
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-28 22:28:08 +0100

    buidl

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,9 +6,5 @@ def find_peak(list_of_integers):
     if not list_of_integers or list_of_integers == []\
       or not isinstance(list_of_integers, list):
         return None
-
-    low = 0
-    high = len(list_of_integers) - 1
-    while low < high:
-        mid = 
-        if list_of_integers[low] > list_of_integers
+    peak = sorted(list_of_integers, reverse=True)
+    return peak[0]
