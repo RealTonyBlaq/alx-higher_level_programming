39f5c76124a621187ccaccbb914b2b2a62ce5cf4
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-28 21:45:57 +0100

    buidl

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,4 +4,5 @@
 def find_peak(list_of_integers):
     if list_of_integers == [] or list_of_integers is None:
         return None
-    peak =
+    peak = list_of_integers.sorted()
+    return peak[-1]
