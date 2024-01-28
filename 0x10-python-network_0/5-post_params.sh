723c383059220f92c3d12d557efd408fb3ed96bc
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-28 21:22:10 +0100

    buidl

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script takes in a URL, sends a POST request to the passed URL, and displays the body of the response
-curl -X POST "" -s "$1"
+curl -X POST "email=test@gmail.com&" -s "$1"
