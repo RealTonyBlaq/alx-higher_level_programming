2c9ca2b62fbe2ba5083bffd061a65f9cb5978758
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-28 17:25:59 +0100

    buidl

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
 # Check if a URL is provided as an argument
-if [ -z "$1" ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
+if [ "$" "$1" ]; then
+    
 url="$1"
 
 # Send a request using curl and display the size of the response body
