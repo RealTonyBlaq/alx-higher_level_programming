7b66d76a541d73cd48913549f12fd9c701096cec
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-28 21:49:48 +0100

    buidl

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,7 +2,7 @@
 """ Script returns the largest number in a list """
 
 def find_peak(list_of_integers):
-    if list_of_integers == [] or list_of_integers is None:
+    if list_of_integers == [] or list_of_integers or li:
         return None
-    peak = list_of_integers.sort().reverse()
-    return peak[0]
+    peak = list_of_integers.sort()
+    return peak
