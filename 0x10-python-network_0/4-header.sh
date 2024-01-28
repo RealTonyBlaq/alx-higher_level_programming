bfd8093c23eb650452e3c9aef7c67d92cfd7322f
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-28 18:49:46 +0100

    buidl

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Script takes in a URL and displays all HTTP methods the server will accept.
-curl -s -I "${1}" | grep "^Allow: .*" | cut -d " " -f 2-
+# Script takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
+curl 
