7bffa2ca567bcdb7c1aceb866688e421a9ff1cda
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-28 18:10:37 +0100

    fix

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script takes in a URL, sends a GET request to the URL, and displays the body of the response
-if [ "$(curl -sI "$1" | head -n 1 | awk '{print $2}')" -eq 200 ]; then 
+if [ "$(curl -sI "$1" | head -n 1 | awk '{print $2}')" -eq 200 ]; then curl -sX GET "$1"; fi
