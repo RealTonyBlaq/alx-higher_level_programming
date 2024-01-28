133be7c595523a5fc293e7992e5c0e7f870c90e3
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-28 17:27:15 +0100

    buidl

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-
 # Check if a URL is provided as an argument
-if [ "$" "$1" ]; then
-    
-url="$1"
 
-# Send a request using curl and display the size of the response body
-body_size=$(curl -sI "$url" | grep -i '^Content-Length:' | awk '{print $2}' | tr -d '\r')
-
-echo "$body_size"
+if [ "$#" -eq 1 ]; then
+    url="$1"
+    # Send a request using curl and display the size of the response body
+    body_size=$(curl -sI "$url" | grep -i '^Content-Length:' | awk '{print $2}' | tr -d '\r')
+    echo "$body_size"
+fi
