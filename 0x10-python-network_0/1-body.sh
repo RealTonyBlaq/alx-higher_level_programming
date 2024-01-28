88cd9d37d1d72efdbe1aec8099eabb488c8114c3
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-28 17:59:59 +0100

    buidl

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script takes in a URL, sends a GET request to the URL, and displays the body of the response
-status_code="$(curl -sI "$1" 
+status_code="$(curl -sI "$1" | head -n 1 | awk '${}
