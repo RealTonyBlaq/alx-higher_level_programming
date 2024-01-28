915ad43973d02bf477f114c7301005b8614e88b9
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-28 18:07:05 +0100

    buidl

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 # Script takes in a URL, sends a GET request to the URL, and displays the body of the response
 status_code="$(curl -sI "$1" | head -n 1 | awk '{print $2}')"
-if [ "$status_code" -eq 200 ]; then
-    curl -sX GET "$1"
-fi
