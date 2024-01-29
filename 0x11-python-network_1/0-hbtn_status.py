28f9a6b1c75f2008a4e27a263d0222b4d635db95
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-29 22:13:16 +0100

    buidl

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,4 +7,4 @@ url = "https://alx-intranet.hbtn.io/status"
 with urlopen(url) as response:
     c = response.read()
     print("Body response:")
-    print(f'\t- type: {type(c)}\n\t- content: {c}\n\t- utf8 content: {c}')
+    print(f'\t- type: {type(c)}\n\t- content: {c}\n\t- utf8 content: {c.decode("ascii")}')
