367df5ce155052cc644a9b6422dd338c50c522fb
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-29 22:39:44 +0100

    fix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,12 +5,9 @@ from urllib.request import urlopen, Request
 
 if __name__ == "__main__":
     url = "https://alx-intranet.hbtn.io/status"
-    try:
-        req = Request(url)
-        with urlopen(req) as response:
-            c = response.read()
-            print("Body response:")
-            print('    - type: {}\n    - content: {}\n    - utf8 content: {}'
-                .format(type(c), c, c.decode("utf-8")))
-    except Exception:
-        pass
+    req = Request(url)
+    with urlopen(req) as response:
+        c = response.read()
+        print("Body response:")
+        print('    - type: {}\n    - content: {}\n    - utf8 content: {}'
+              .format(type(c), c, c.decode("utf-8")))
