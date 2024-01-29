6a0bdd435dbf8de6257774b31a8a44070957a411
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-29 22:18:19 +0100

    fix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """ Script that fetches a url """
-if __name__ == "__main__":
-from urllib.request import urlopen
 
-url = "https://alx-intranet.hbtn.io/status"
-with urlopen(url) as response:
-    c = response.read()
-    print("Body response:")
-    print(f'\t- type: {type(c)}\n\t\
-          - content: {c}\n\t- utf8 content: {c.decode("ascii")}')
+if __name__ == "__main__":
+    from urllib.request import urlopen
+
+    url = "https://alx-intranet.hbtn.io/status"
+    with urlopen(url) as response:
+        c = response.read()
+        print("Body response:")
+        print(f'\t- type: {type(c)}\n\t\
+            - content: {c}\n\t- utf8 content: {c.decode("ascii")}')
