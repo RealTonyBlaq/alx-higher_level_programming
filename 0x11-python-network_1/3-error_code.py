a29adc10b448946b10ac707f5c19aa3ce359e6f6
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-30 10:51:37 +0100

    fix

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -5,7 +5,8 @@ sends a POST request to the passed URL with the email as a parameter,
 and displays the body of the response (decoded in utf-8).
 """
 
-import urllib
+import urllib.request
+import urllib.error
 from sys import argv
 
 
