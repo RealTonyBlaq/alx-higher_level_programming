ecbc024629dd1e65d418283f2fff45d111bd02e0
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-30 09:51:10 +0100

    buidl

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,3 +5,9 @@ the passed URL with the email as a parameter, and displays
 the body of the response (decoded in utf-8).
 """
 
+from urllib.request import Request, urlopen
+from sys import argv
+
+
+if __name__ == "__main__":
+    
