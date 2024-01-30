26c8a5941809a999829d2e11f405cce1b52f5f67
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-30 10:33:19 +0100

    bidl

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-
 """
-Script takes in a URL and an email, sends a POST request to
-the passed URL with the email as a parameter, and displays
-the body of the response (decoded in utf-8).
+Script takes in a URL and an email,
+sends a POST request to the passed URL with the email as a parameter,
+and displays the body of the response (decoded in utf-8).
 """
 
-from urllib.request import Request, urlopen
-import urllib.parse
+import urllib.request import Request, urlopen
 from sys import argv
 
 
