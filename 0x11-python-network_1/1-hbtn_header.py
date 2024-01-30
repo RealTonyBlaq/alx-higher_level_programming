361b3e416d9d6d935a99c2745676c7bac37cd966
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-30 08:37:34 +0100

    buidl

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -10,4 +10,6 @@ from urllib.request import Request, urlopen
 
 if __name__ == "__main__":
     req = Request(sys.argv[1])
-    with open(req)
+    with urlopen(req) as response:
+        r = response.read()
+        print(type(r))
