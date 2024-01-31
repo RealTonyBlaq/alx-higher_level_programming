919b0fb6caba38e25fa30fe2b032da5709457c8f
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-31 22:04:13 +0100

    fix

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -11,6 +11,6 @@ from sys import argv
 
 if __name__ == "__main__":
     URL = "https://api.github.com/repos/{}/{}/commits"\
-        .format(argv[2], argv[1])
+        .format(argv[1], argv[1])
     r = requests.get(URL)
-    
+    print(r.json())
