6848b6e7737c6a7dca8dbb0efafa49dfaa8b7743
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-31 14:31:04 +0100

    all

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -2,22 +2,22 @@
 """
 Script takes your GitHub credentials (username and password)
 and uses the GitHub API to display your id.
-argv[1] - Username
-argv[2] - password (Personal Access Token)
+argv[1] - Repository name
+argv[2] - owner name
 """
 import requests
 from sys import argv
 
 
 if __name__ == "__main__":
-    URL = "https://api.github.com/user"
-    credentials = (argv[1], argv[2])
-    r = requests.get(url=URL, auth=credentials)
+    URL = "https://api.github.com/user/{}/{}.git"\
+        .format(argv[2], argv[1])
+    r = requests.get(URL)
     try:
         r.raise_for_status()
 
         data = r.json()
         userID = data.get("id")
-        print(userID) if userID else print("None")
+        
     except Exception:
         print("None")
