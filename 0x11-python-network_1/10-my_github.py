515ca828cfddd7478f3e2b0e67499b446a7eaa22
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-31 14:01:43 +0100

    display id using GitHub API

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -2,12 +2,13 @@
 """
 Script takes your GitHub credentials (username and password)
 and uses the GitHub API to display your id.
+argv[1] - User
 """
 import requests
 from sys import argv
 
 
 if __name__ == "__main__":
-     = {"email": argv[2]}
+    credentials = (argv[1], argv[2])
     r = requests.post(url=argv[1], data=data)
     print(r.text)
