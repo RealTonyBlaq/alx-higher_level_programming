1bf95f177f4f1515c1dccef1882d5d452e04ddf2
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-31 13:39:16 +0100

    try

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -22,5 +22,5 @@ if __name__ == "__main__":
             print("[{}] {}".format(id, name))
         else:
             print("No result")
-    else:
+    except requests.exceptions.JSONDecodeError:
         print("Not a valid JSON")
