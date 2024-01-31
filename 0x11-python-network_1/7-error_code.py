705628b0e8dbb2b54534c629ea20237af4922caf
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-31 12:29:08 +0100

    fix

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -10,4 +10,4 @@ from sys import argv
 if __name__ == "__main__":
     r = requests.get(argv[1])
     if r.status_code >= 400:
-        print("Error code:", r.status_code)
+        print("Error code: ", r.status_code)
