b69be7008129893121847215d656bee8316eef7a
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-31 12:36:53 +0100

    buidl

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Script takes in a URL, sends a request to the URL
+and displays the value of the variable X-Request-Id
+in the response header
+"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = ""
+    r = requests.get()
+    print(r.headers.get("X-Request-Id"))
