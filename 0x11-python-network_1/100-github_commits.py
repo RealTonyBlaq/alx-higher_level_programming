6e423f75dd8c88ed50587301afef89fd0584f78e
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-31 14:27:07 +0100

    all

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Script takes your GitHub credentials (username and password)
+and uses the GitHub API to display your id.
+argv[1] - Username
+argv[2] - password (Personal Access Token)
+"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    URL = "https://api.github.com/user"
+    credentials = (argv[1], argv[2])
+    r = requests.get(url=URL, auth=credentials)
+    try:
+        r.raise_for_status()
+
+        data = r.json()
+        userID = data.get("id")
+        print(userID) if userID else print("None")
+    except Exception:
+        print("None")
