38e57a4c15de85b5d842034ae73bd3f318fabcb0
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-03-03 00:01:43 +0100

    rad contents

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -6,6 +6,7 @@ const reader = require('fs');
 reader.readFile(process.argv[2], 'utf-8', (err, contents) => {
   if (err) {
     console.error(err);
+    return;
   } else {
     console.log(contents);
   }
