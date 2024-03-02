dc1c480761e43772ef2ffe7f2da85b7ca06dec3a
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-03-03 00:43:04 +0100

    fs

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,14 +1,13 @@
 #!/usr/bin/node
 /* Script displays the status code of a GET request */
 
-const { error } = require('console');
 const req = require('request');
 const URL = process.argv[2];
 
 req.get(URL, (error, response) => {
   if (error) {
-    console.errror(error)
+    console.errror(error);
     return;
   }
-  console.log('code:', response.statusCode)
+  console.log('code:', response.statusCode);
 });
