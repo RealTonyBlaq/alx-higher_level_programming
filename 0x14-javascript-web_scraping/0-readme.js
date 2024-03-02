027292dcb952f230e33db9c31ab037dfa4fa2055
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-03-02 23:55:16 +0100

    rad contents

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,7 +2,6 @@
 /* Script reads content from a file */
 
 const reader = require('fs')
-reader.readFile(process.argv[1], 'utf-8', { err, contents}) => {
-
-}
-
+reader.readFile(process.argv[1], 'utf-8', { err, contents} => {
+    
+})
