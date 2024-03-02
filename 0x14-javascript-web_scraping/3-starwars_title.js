244342b151e978cd1a69e3697ea223fe5b45ad4f
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-03-03 00:49:35 +0100

    get id from API

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,2 +1,7 @@
 #!/usr/bin/node
-/* Script prints the title of a Star Wars movie where the episode number matches a given integer
+/*
+Script prints the title of a Star Wars movie
+where the episode number matches a given integer
+*/
+
+const request = require('')
