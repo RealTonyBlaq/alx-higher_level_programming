92732f062ec9f0045c3296a8c283cc1e492eab57
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-03-03 00:37:00 +0100

    use callback fxn to print

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
 /* Script displays the status code of a GET request */
 
+const { error } = require('console');
 const req = require('request');
 const URL = process.argv[2];
-const response = req.get(URL);
 
-console.log('code:', response.statusCode);
+req.get(URL, (error, response, body) => {
+  console.log("code:", response.statusCode)
+});
