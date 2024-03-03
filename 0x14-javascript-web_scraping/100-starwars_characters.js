ccda24738cd2c5be3df5a66068f5ae85f8c12db3
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-03-03 23:10:03 +0100

    characters of the Jedi

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -19,10 +19,13 @@ request.get(filmURL, (error, response, body) => {
     for (const character of data.characters) {
       const list = character.split('/');
       const id = list[list.length - 2];
+
       request.get(peopleURL + `${id}`, (error, response, body) => {
         if (error) {
-          console.error
+          console.error(error);
+          return;
         }
+        
       })
     }
  }
