638c218e6c96515e7d99046564fc67936b156033
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-03-03 01:39:42 +0100

    counter

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -15,7 +15,7 @@ request(URL, (error, response, body) => {
   if (response.statusCode === 200) {
     const data = JSON.parse(body);
     for (d of data) {
-        if (d.url.split('/')[-1] )
+        if (d.url.split('/')[-1] === )
     }
   }
 });
