20d61bdac3ab0302798955a1634cd28c462d69db
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-03-03 01:15:38 +0100

    get id from API

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -8,11 +8,11 @@ const request = require('request');
 const id = Number(process.argv[2]);
 const URL = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
-request.get(URL, (error, response) => {
+request.get(URL, (error, response, body) => {
   if (error) {
     console.error(error);
     return;
   }
-  data = JSON.parse();
-  console.log(data.body["title"]);
+  data = JSON.parse(body);
+  console.log(data["title"]);
 });
