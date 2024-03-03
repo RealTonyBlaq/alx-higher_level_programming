0f2097a709405151d5a8ecac008bd84801b8ea60
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-03-03 16:12:46 +0100

    computes number of tasks by user_id

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,3 +1,8 @@
 #!/usr/bin/node
-/* Script computes the number of tasks completed by user id */
+/*
+Script computes the number of tasks completed by user id
+Accepts one argument:
+    The API URI
+*/
+
 
