4aaafd00b54e21e7839547a4509a2fc1c94687ac
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-03-03 15:58:59 +0100

    storing lorem api response to file

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -17,6 +17,6 @@ request.get(URL, (error, response, body) => {
     return;
   }
   if (response.statusCode === 200) {
-    fs.writeFile(filename, body);
+    fs.writeFile(filename, body, 'utf-8');
   }
 });
