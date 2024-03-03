8e7152562778f17dc28bd30a8974314efe1f0c64
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-03-03 16:45:55 +0100

    computes number of tasks by user_id

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -15,8 +15,7 @@ request.get(URI, (error, response, body) => {
   }
   if (response.statusCode === 200) {
     const data = JSON.parse(body);
-    
-    // Using reduce to count completed tasks for each user
+
     const tasks = data.reduce((acc, task) => {
       const userID = task.userId;
       if (task.completed === true) {
