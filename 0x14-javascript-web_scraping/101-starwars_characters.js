c41f63b75cb9e0cfa390097e2ba508944fbd5641
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-03-03 23:56:56 +0100

    fixing code

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -29,15 +29,8 @@ request.get(filmURL, (error, response, body) => {
         }
         if (response.statusCode === 200) {
           const person = JSON.parse(body);
-          charactersList.push(person.name);
-
-          if (charactersList.length === data.characters.length) {
-            charactersList.forEach((elem) => console.log(elem));
-          }
         }
       });
     };
-
-    data.characters.forEach(fetchCharacterDetails);
   }
 });
