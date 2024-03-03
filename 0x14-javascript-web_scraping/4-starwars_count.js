134fa035330ae5286ac9bf8eca451e87cacf3f0d
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-03-03 15:31:03 +0100

    fixing code

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -15,7 +15,7 @@ request(URL, (error, response, body) => {
   if (response.statusCode === 200) {
     const data = JSON.parse(body);
     let count = 0;
-    console.log(data.re);
+    for (dic in )
     console.log(count);
   }
 });
