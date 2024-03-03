c42b417391ffe624067a2ec9be4cf817cddbfd52
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-03-03 16:23:27 +0100

    computes number of tasks by user_id

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -17,7 +17,8 @@ request.get(URL, (error, response, body) => {
     data = JSON.parse(body);
     tasks = {};
     for (dict of data) {
-      userID = dict.
+      userID = dict.userID
+      
     }
   }
 })
