0c800cac16baa0397d27d2db27337031f9401c45
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-03-03 23:21:42 +0100

    characters of the Jedi

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -7,15 +7,15 @@ Script that prints all characters of a Star Wars movie:
 const request = require('request');
 const movieID = process.argv[2];
 const filmURL = `https://swapi-api.alx-tools.com/api/films/${movieID}`;
-const peopleURL = `https://swapi-api.alx-tools.com/api/people/`;
+const peopleURL = 'https://swapi-api.alx-tools.com/api/people/';
 
 request.get(filmURL, (error, response, body) => {
- if (error) {
+  if (error) {
     console.error(error);
     return;
- }
- if (response.statusCode === 200) {
-    const data = JSON.parse(body)
+  }
+  if (response.statusCode === 200) {
+    const data = JSON.parse(body);
     for (const character of data.characters) {
       const list = character.split('/');
       const id = list[list.length - 2];
@@ -31,5 +31,5 @@ request.get(filmURL, (error, response, body) => {
         }
       });
     }
- }
+  }
 });
