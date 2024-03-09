b510924f51f2f5b0616f09faf174fed2a9a2f49c
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-03-09 14:07:41 +0100

    get titles

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -2,11 +2,11 @@ function listTitles () {
   $.ajax({
     url: 'https://swapi-api.alx-tools.com/api/films/?format=json',
     method: 'GET',
-    dataType: 'json':
+    dataType: 'json',
     success: function (data) {
       for (const result of data.results) {
-        
+        const title = da
       }
     }
-  })
+  });
 }
