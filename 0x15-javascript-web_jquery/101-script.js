35d65e7d35425220ac0e6bcce4d8dd99594e9962
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-03-09 17:39:02 +0100

    adds to or removes from a list

--- 0x15-javascript-web_jquery/101-script.js
+++ 0x15-javascript-web_jquery/101-script.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
     $('UL.my_list').append(item);
   });
   $('DIV#remove_item').on('click', function () {
-    $('UL.my_list').remove('li':last)
-  })
+    $('UL.my_list').remove('li:last');
+  });
+  $()
 });
