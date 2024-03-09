68f9a24d58651b2298e809f6b9d8121bed9eb675
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-03-09 13:53:24 +0100

    gets an api

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,7 +1,5 @@
 function getData () {
-  $.get('https://swapi-api.alx-tools.com/api/people/5/?format=json', function (data, textStatus) {
-    alert('Response' + data + ' with status: ' + textStatus);
-  })
+  $.ajax()
 }
 
 $(document).ready(function () {
