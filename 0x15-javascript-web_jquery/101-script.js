04b6e08f45acfb646e15489b312d8c7e4763f7d4
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-03-09 17:31:37 +0100

    adds to or removes from a list

--- 0x15-javascript-web_jquery/101-script.js
+++ 0x15-javascript-web_jquery/101-script.js
@@ -2,5 +2,6 @@ $(document).ready(function () {
   const item = $('<li></li>').text('Item');
   $('DIV#add_item').on('click', function () {
     $('UL.my_list').append(item);
-  })
+  });
+  $('DIV#remove_item').on('click', function)
 });
