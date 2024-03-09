f3a0304ca82020a04e74895660f60210467b1a52
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-03-09 12:44:15 +0100

    jquery

--- 0x15-javascript-web_jquery/3-script.js
+++ 0x15-javascript-web_jquery/3-script.js
@@ -1,5 +1,9 @@
 function addRedClass () {
-  if (! $('header').hasClass('red') {
+  if (! $('header').hasClass('red')) {
     $('header').addClass('red');
   }
 }
+
+$(document).ready(function () {
+  
+})
