601afd30449c729db4e737f49c9ea0383a4441c0
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-03-09 13:00:04 +0100

    toggles class

--- 0x15-javascript-web_jquery/4-script.js
+++ 0x15-javascript-web_jquery/4-script.js
@@ -1,5 +1,5 @@
 function toggleClass () {
   if ($('header').hasClass('red') && $('header').hasClass('blue')) {
     $('header').removeClass('blue');
-  }
+  } else if ($('header').hasClass('red'))
 }
