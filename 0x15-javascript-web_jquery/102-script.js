c98c0d19b98e565c376d3555fc51a505f9725ca1
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-03-09 18:07:11 +0100

    translate

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
-  $('INPUT#btn_translate')
-  const langCode = $('INPUT#language_code').attr();
+  $('INPUT#btn_translate').on('click', function () {
+    const langCode = $('INPUT#language_code').attr();
   $.ajax({
     url: `https://www.fourtonfish.com/hellosalut/?lang=${langCode}`,
     method: 'GET',
@@ -9,5 +9,5 @@ $(document).ready(function () {
       
     }
   })
-
+  });
 });
