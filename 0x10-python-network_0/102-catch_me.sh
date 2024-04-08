305aa721327245833b7a059457dbad7552789be9
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-04-08 22:58:37 +0100

    forces server to send a message

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Force a server's hand to respond with "You got me!"
-curl -X POST -s -H "X-Custom-Header: You got me!" http://0.0.0.0:5000/catch_me
+curl -X POST -s -H "X-Custom-Header: You got me!" 0.0.0.0:5000/catch_me
