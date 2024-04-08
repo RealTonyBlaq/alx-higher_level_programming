8704d80b2547bb2442918401f82a46e1e913ca3f
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-04-08 23:07:05 +0100

    forces server to send a message

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Force a server's hand to respond with "You got me!"
-curl -X PUT -sL 0.0.0.0:5000/catch_me -H "Origin:HolbertonSchool"
+curl -s -L 0.0.0.0:5000/catch_me -H "You got me!"
